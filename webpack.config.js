d2fc8e897faf98dd138cf82708e994939f247ed3
amiraallagui <allagui_fawzi@outlook.fr>
2023-11-09 23:46:32 +0100

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,5 @@
 const path = require('path');
-module.exports = {
-    entry:"./src/index.js",
-    output:{
-        filename:"bundle.js",
-        path:path.resolve(__dirname,"./dist"),
-    },
-    mode:'none'
+
+exports.module = {
+    entry : ""
 }
