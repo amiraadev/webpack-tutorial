77834b3c0289840c934451502702ab67a90481d0
amiraallagui <allagui_fawzi@outlook.fr>
2023-11-10 18:03:39 +0100

    adding the style file

--- src/components/hello-world-button/hello-world-button.js
+++ src/components/hello-world-button/hello-world-button.js
@@ -2,11 +2,13 @@ class helloWorldButton {
     render(){
         const button = document.createElement('button');
         button.innerHTML = 'Hello World';
+        button.classList.add('hello-world-button');
         const body = document.querySelector('body');
         // Use 'addEventListener' instead of 'onClick'
         button.addEventListener('click', function () {
             const p = document.createElement('p'); // Specify the tag name 'p'
             p.innerHTML = 'Hello everyone! We are learning webpack'; // Fix the innerHTML
+            p.classList.add('hello-world-text');
             body.appendChild(p);
         });
         body.appendChild(button);
