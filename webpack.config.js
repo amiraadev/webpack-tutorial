f88c74bf3b8714a46ddc9edca5f406639a4d5bb6
amiraallagui <allagui_fawzi@outlook.fr>
2023-11-10 09:05:39 +0100

    adding the build script to package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path'; // Add this line
 
-const path = require('path');
-module.exports = {
-    entry:"./src/index.js",
-    output:{
-        filename:"bundle.js",
-        path:path.resolve(__dirname,"./dist"),
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, './dist'),
     },
-    mode:'none'
-}
+    mode: 'none',
+};
