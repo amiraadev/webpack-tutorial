07bc03866546184f1ab49a4b049a2a4bc52235f3
amiraallagui <allagui_fawzi@outlook.fr>
2023-11-11 17:49:41 +0100

    adding [contenthash] to filename so that it changes its name every time the code changes

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ const __dirname = dirname(__filename);
 export default {
     entry: './src/index.js',
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[contenthash].js',
         path: path.resolve(__dirname, './dist'),
         publicPath: 'dist/'
     },
