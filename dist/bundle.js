f18e72af0197f3d64e02601ebf267328327e0301
amiraallagui <allagui_fawzi@outlook.fr>
2023-11-11 15:53:48 +0100

    rule for babel

--- dist/bundle.js
+++ dist/bundle.js
@@ -10,25 +10,37 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
 /* harmony import */ var _hello_world_button_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(2);
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+function _toPropertyKey(arg) { var key = _toPrimitive(arg, "string"); return _typeof(key) === "symbol" ? key : String(key); }
+function _toPrimitive(input, hint) { if (_typeof(input) !== "object" || input === null) return input; var prim = input[Symbol.toPrimitive]; if (prim !== undefined) { var res = prim.call(input, hint || "default"); if (_typeof(res) !== "object") return res; throw new TypeError("@@toPrimitive must return a primitive value."); } return (hint === "string" ? String : Number)(input); }
 
-class helloWorldButton {
-    render(){
-        const button = document.createElement('button');
-        button.innerHTML = 'Hello World';
-        button.classList.add('hello-world-button');
-        const body = document.querySelector('body');
-        // Use 'addEventListener' instead of 'onClick'
-        button.addEventListener('click', function () {
-            const p = document.createElement('p'); // Specify the tag name 'p'
-            p.innerHTML = 'Hello everyone! We are learning webpack'; // Fix the innerHTML
-            p.classList.add('hello-world-text');
-            body.appendChild(p);
-        });
-        body.appendChild(button);
+var helloWorldButton = /*#__PURE__*/function () {
+  function helloWorldButton() {
+    _classCallCheck(this, helloWorldButton);
+  }
+  _createClass(helloWorldButton, [{
+    key: "render",
+    value: function render() {
+      var button = document.createElement('button');
+      button.innerHTML = 'Hello World';
+      button.classList.add('hello-world-button');
+      var body = document.querySelector('body');
+      // Use 'addEventListener' instead of 'onClick'
+      button.addEventListener('click', function () {
+        var p = document.createElement('p'); // Specify the tag name 'p'
+        p.innerHTML = 'Hello everyone! We are learning webpack'; // Fix the innerHTML
+        p.classList.add('hello-world-text');
+        body.appendChild(p);
+      });
+      body.appendChild(button);
     }
-}
-
-/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (helloWorldButton); 
+  }]);
+  return helloWorldButton;
+}();
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (helloWorldButton);
 
 /***/ }),
 /* 2 */
@@ -536,23 +548,14 @@ var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _components_hello_world_button_hello_world_button_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
 
-
-const helloButton = new _components_hello_world_button_hello_world_button_js__WEBPACK_IMPORTED_MODULE_0__["default"]();
+var helloButton = new _components_hello_world_button_hello_world_button_js__WEBPACK_IMPORTED_MODULE_0__["default"]();
 helloButton.render();
-
-
-
-
-
-
-
 
 // import helloWorld from './hello-world.js' ;
 // import addImage from './add-image.js' ;
 
 // helloWorld();
 // addImage();
-
 })();
 
 /******/ })()
