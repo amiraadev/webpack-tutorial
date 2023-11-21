6695e8ae30e12acfb813280e17c872808ef8eb16
amiraallagui <allagui_fawzi@outlook.fr>
2023-11-21 12:44:04 +0100

    adding a parametre to the Heading

--- src/components/heading/heading.js
+++ src/components/heading/heading.js
@@ -1,9 +1,9 @@
 import "./heading.scss";
 class Heading {
-    render() {
+    render(custom_title) {
         const h1 = document.createElement('h1');
         const body = document.querySelector('body');
-        h1.innerHTML ="webpack is awesome";
+        h1.innerHTML ="webpack is awesome "+custom_title;
         body.appendChild(h1);
     }
 }
